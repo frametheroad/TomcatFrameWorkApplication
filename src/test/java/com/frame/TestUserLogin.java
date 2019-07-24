7a064e1dcc28cbc6dae6617624e51eafe99a6753
Contents2000 <markinemail@163.com>
2019-07-25 00:57:49 +0800

    添加Junit

---
+++ src/test/java/com/frame/TestUserLogin.java
@@ -0,0 +1,30 @@
+package com.frame;
+
+import com.frame.BeanVo.UserVo;
+import com.frame.Controller.UserLogin;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestUserLogin {
+    private static final Logger logger = LoggerFactory.getLogger(TestUserLogin.class);
+    @Autowired
+    UserLogin userLogin;
+    @Test
+    public void login(){
+        UserVo user = new UserVo();
+        user.setName("zhangsan");
+        user.setPassword("zs123");
+        logger.info(userLogin.login(user).toString());
+    }
+    @Test
+    public void list(){
+        logger.info(userLogin.list().toString());
+    }
+}
